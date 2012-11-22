bd1397639f5eb51ec20a8b005fc65f35a3a0493d
Saverio Proto <zioproto@gmail.com>
2012-11-22 14:36:38 +0100

    Fixing syntax problems from the MOCA2012 codesprint

--- rrdgraphs/views.py
+++ rrdgraphs/views.py
@@ -8,22 +8,27 @@ from rrdgraphs.models import Graph
 import rrdtool
 import os
 
-def graph_link(request, id):
+def link(request, link_id):
     try:
-        link = Link.objects.only('from_interface', 'to_interface').get(pk=id)
+        link = Link.objects.only('from_interface', 'to_interface').get(pk=link_id)
     except ObjectDoesNotExist:
         raise Http404
 
-    from_graph = link.from_interface.graph.draw_graph
-    to_graph = link.to_interface.graph.draw_graph
+    #TODO: in future handle this two in two try catch blocks ?
+    try:
+        from_graph = link.from_interface.graph.draw_graph
+        to_graph = link.to_interface.graph.draw_graph
+    except ObjectDoesNotExist:
+        raise Http404
 
-    RRD_DIR = '%s/rrd/' % os.path.dirname(os.path.realpath(__file__))
+
+    RRD_DIR = '%s/../rrd/' % os.path.dirname(os.path.realpath(__file__))
     rrd_from = '%s/%s.rrd' % (RRD_DIR, from_interface)
     rrd_from_png = '%s/rrd/%s.png' % (settings.MEDIA_ROOT, from_interface)
     rrd_to = '%s/%s.rrd' % (RRD_DIR, to_interface)
     rrd_to_png = '%s/rrd/%s.png' % (settings.MEDIA_ROOT, to_interface)
 
-    rddtool.graph(rrd_from_png, 
+    rrdtool.graph(rrd_from_png,
         '--imgformat', 'PNG',
         '--width', '720',
         '--height', '140',
@@ -31,7 +36,7 @@ def graph_link(request, id):
         '--end', 'now',
         'DEF:graph_out_pre=%s:out:LAST' % rrd_from)
 
-    rddtool.graph(rrd_to_png, 
+    rrdtool.graph(rrd_to_png,
         '--imgformat', 'PNG',
         '--width', '720',
         '--height', '140',
@@ -39,7 +44,5 @@ def graph_link(request, id):
         '--end', 'now',
         'DEF:graph_out_pre=%s:out:LAST' % rrd_to)
      
-     return HttpResponse('<img src="%s" /> <img src="%s">' % (rrd_from_png, rrd_to_png)
-
-
+    return HttpResponse('<img src="%s" /> <img src="%s">' % (rrd_from_png, rrd_to_png))
 
